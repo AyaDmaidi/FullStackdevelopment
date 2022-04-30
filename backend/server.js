44b5ae3b303fbee633c40114c3983c4660f8b786
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-04-30 23:07:31 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
 import productRouter from './routers/productRouter.js';
 
 app.use('/api/products', productRouter);
+import dotenv from 'dotenv';
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
