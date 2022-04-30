067a5454ae35176e49651d29900f96ba2440aa35
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-04-30 23:52:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import {BrowserRouter , Route} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { signout } from './actions/userActions';
+const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+ </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )} 
+
 function App() {
   return (
 
