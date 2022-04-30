66a506d0bf91c76daf8db06c6d610d4f8e9e628a
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-05-01 00:45:30 +0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,3 +1,4 @@
+
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
@@ -6,6 +7,10 @@ import {
   productListReducer,
 } from './reducers/productReducers';
 import { userSigninReducer } from './reducers/userReducers';
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
   userSignin: {
@@ -24,6 +29,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
