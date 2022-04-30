082bc92f1c6d58163e1d165631b02eb172e7308e
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-04-30 23:28:09 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import SigninScreen from './screens/SigninScreen';
+
+  <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+ </main>
