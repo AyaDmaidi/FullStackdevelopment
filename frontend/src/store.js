e8b075fb95ebdcab522365ad8f68522bc3de7acf
AyaDmaidi <92691434+AyaDmaidi@users.noreply.github.com>
2022-05-02 04:45:04 +0300

    store Update "Aya"

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,35 @@
-
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import {
+  orderCreateReducer,
+  orderDeleteReducer,
+  orderDeliverReducer,
+  orderDetailsReducer,
+  orderListReducer,
+  orderMineListReducer,
+  orderPayReducer,
+  orderSummaryReducer,
+} from './reducers/orderReducers';
+import {
+  productCategoryListReducer,
+  productCreateReducer,
+  productDeleteReducer,
   productDetailsReducer,
   productListReducer,
+  productReviewCreateReducer,
+  productUpdateReducer,
 } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducers';
 import {
+  userAddressMapReducer,
+  userDeleteReducer,
+  userDetailsReducer,
+  userListReducer,
   userRegisterReducer,
   userSigninReducer,
+  userTopSellerListReducer,
+  userUpdateProfileReducer,
+  userUpdateReducer,
 } from './reducers/userReducers';
 
 const initialState = {
@@ -22,11 +42,10 @@ const initialState = {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
-  },
-};
- shippingAddress: localStorage.getItem('shippingAddress')
+    shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: 'PayPal',
   },
 };
 
@@ -36,19 +55,33 @@ const reducer = combineReducers({
   cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderMineList: orderMineListReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userUpdate: userUpdateReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productDelete: productDeleteReducer,
+  orderList: orderListReducer,
+  orderDelete: orderDeleteReducer,
+  orderDeliver: orderDeliverReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userTopSellersList: userTopSellerListReducer,
+  productCategoryList: productCategoryListReducer,
+  productReviewCreate: productReviewCreateReducer,
+  userAddressMap: userAddressMapReducer,
+  orderSummary: orderSummaryReducer,
 });
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    productList: productListReducer,
-  productDetails: productDetailsReducer,
-  cart: cartReducer,
-  userSignin: userSigninReducer,
-  userRegister: userRegisterReducer,
-});
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
+
 export default store;
