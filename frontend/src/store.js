b94a802ba6f9f95d332a71642766871ae6329ce3
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-05-02 05:46:59 +0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -83,5 +83,15 @@ const store = createStore(
   initialState,
   composeEnhancer(applyMiddleware(thunk))
 );
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
+    paymentMethod: 'PayPal',
+  },
+};
 
 export default store;
